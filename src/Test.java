a367e5ef7d88cc38fbbfd33305b2954ceecfff27
ozzi- <oz@oz-web.com>
2018-04-10 16:41:53 +0200

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+	public static void main(String[] args){
+		// %VERSION will be replaced by ant using the variable von build.properties
+		System.out.println("Hello world - Version: 1.0");
+	}
+}
